a34a7be6b6ab2b4fc9d91db0f7d1b33ff215bba0
Gustavo Pilar <pilargustavo7@gmail.com>
2025-03-26 21:24:21 -0300

    Fix: Ajustando as pastas do layout

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -6,14 +6,14 @@ import { BadgeModule } from 'primeng/badge';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { RippleModule } from 'primeng/ripple';
-import { AppMenuComponent } from './app.menu.component';
-import { AppMenuitemComponent } from './app.menuitem.component';
+import { AppMenuComponent } from './menu/app.menu.component';
+import { AppMenuitemComponent } from './menuitem/app.menuitem.component';
 import { RouterModule } from '@angular/router';
-import { AppTopBarComponent } from './app.topbar.component';
-import { AppFooterComponent } from './app.footer.component';
+import { AppTopBarComponent } from './topbar/app.topbar.component';
+import { AppFooterComponent } from './footer/app.footer.component';
 import { AppConfigModule } from './config/config.module';
-import { AppSidebarComponent } from "./app.sidebar.component";
-import { AppLayoutComponent } from "./app.layout.component";
+import { AppSidebarComponent } from "./sidebar/app.sidebar.component";
+import { AppLayoutComponent } from "./layout/app.layout.component";
 import { CommonModule } from '@angular/common';
 
 @NgModule({
